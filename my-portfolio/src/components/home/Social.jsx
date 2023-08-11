6764053e32f6ea43f1d25b78630cda208baea43c
Wan Xin <65882857+wanxin1996@users.noreply.github.com>
2023-08-12 01:52:01 +0800

    completed home component

---
+++ my-portfolio/src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.linkedin.com/in/wan-xin-454b4111b/" className="home__social-icon" target='_blank'>
+                <i class="uil uil-linkedin-alt"></i>
+            </a>
+            <a href="https://t.me/wanxin0" className="home__social-icon" target='_blank'>
+                <i class="uil uil-telegram-alt"></i>
+            </a>
+            <a href="https://github.com/wanxin1996" className="home__social-icon" target='_blank'>
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
