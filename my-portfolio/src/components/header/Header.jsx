96b686366103695de3f9ba92fc74bd8577a8bb63
Wan Xin <65882857+wanxin1996@users.noreply.github.com>
2023-08-18 15:33:48 +0800

    change package

--- my-portfolio/src/components/header/Header.jsx
+++ my-portfolio/src/components/header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
     return (
         <header className="header">
             <nav className="nav container">
-                <a href="index.html" className="nav__logo">Wan</a>
+                <a href="index.html" className="nav__logo">Wan Xin</a>
 
                 <div className={Toggle ? 'nav__menu show-menu' : 'nav__menu'}>
                     <ul className="nav__list grid">
